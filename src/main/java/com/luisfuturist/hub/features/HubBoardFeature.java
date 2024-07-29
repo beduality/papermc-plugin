592ef2efc0050c7df1daf4c5fa2f2c57453a4685
Luis Emidio <luisfuturist@gmail.com>
2024-07-28 22:28:18 -0300

    fix: update hub scoreboard players num

--- src/main/java/com/luisfuturist/hub/features/HubBoardFeature.java
+++ src/main/java/com/luisfuturist/hub/features/HubBoardFeature.java
@@ -1,23 +1,36 @@
 package com.luisfuturist.hub.features;
 
-import org.bukkit.Bukkit;
-
 import com.luisfuturist.core.features.BoardFeature;
+import com.luisfuturist.core.models.User;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.md_5.bungee.api.ChatColor;
 
 public class HubBoardFeature extends BoardFeature {
-    
-    private void setupBoard() {
-        var orchestrator = getPhase().getGame().getOrchestrator();
-        
+
+    private void setupBoard() { 
         var displayName = Component.text().color(NamedTextColor.BLUE)
                 .append(Component.text("BED Hub")).asComponent();
 
         getObjective().displayName(displayName);
+    }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupBoard();
+    }
+
+    @Override
+    public void onJoin(User user) {
+        super.onJoin(user);
+        updateBoard();
+    }
+
+    public void updateBoard() {
+        var orchestrator = getPhase().getGame().getOrchestrator();
+        
         var playersNum = orchestrator.getGlobal().getPlayers().size();
         var maxPlayers = orchestrator.getGlobal().getMaxPlayers();
 
@@ -25,11 +38,5 @@ public class HubBoardFeature extends BoardFeature {
                 ChatColor.GRAY + "Online",
                 playersNum + "/" + maxPlayers
         });
-    }
-
-    @Override
-    public void onEnable() {
-        super.onEnable();
-        setupBoard();
     }
 }
